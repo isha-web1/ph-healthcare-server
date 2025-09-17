3c6b07d10f01851c187c09ff17b7c90aba1f3c4e
isha-web1 <isha.web1@gmail.com>
2025-09-17 22:09:04 +0600

    complete searching,sorting,filter and pagination adminData

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -1,9 +1,14 @@
 import { Request, Response } from "express";
 import { adminService } from "./admin.service";
+import pick from "../../../shared/pick";
+import { adminFilterableFields } from "./admin.constant";
 
 const getAllFromDb = async (req: Request, res: Response) => {
   try {
-    const result = await adminService.getAllFromDb(req.query);
+   const filters = pick(req.query, adminFilterableFields);
+    const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder'])
+    console.log(options)
+    const result = await adminService.getAllFromDb(filters, options)
     res.status(200).json({
       success: true,
       message: "Admin Data Fetched Successfully",
